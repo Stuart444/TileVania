813549ad6d031a6dd071c9725c6f7aede1a71683
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-09-05 00:32:27 +0100

    Character Moving Left/Right implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    Rigidbody2D myRigidbody;
+
+    [SerializeField] float runSpeed = 5f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    private void Run()
+    {
+        // Value is between -1 to +1
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+
+        Vector2 playerVelocity = new Vector2(controlThrow * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+    private void FlipSprite()
+    {
+        // if Velocity is greater than Zero (Epsilon) then true
+        bool playerHasHorizontalSpeed = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            // returns 1 if x is positive or zero otherwise it returns -1
+            transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+        }
+    }
+}
