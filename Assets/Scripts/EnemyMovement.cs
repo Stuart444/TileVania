094080353a9f8afdc6aed11cce4e90cf17cfbb1d
Stuart Gannon <stuart_gannon444@yahoo.co.uk>
2018-09-12 12:09:03 +0100

    Enemy set up and moving back and forth

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D myRigidbody;
+    Animator myAnimator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (IsFacingRight())
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, 0f);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, 0f);
+        }
+	}
+
+    /// <summary>
+    /// checks if the enemy is Facing right if the local scale is greater than 0
+    /// </summary>
+    /// <returns>true if facing right, false otherwise</returns>
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // flips the local scale, if plus, it becomes a minus and if minus, it becomes
+        // a plus because two minuses make a plus
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+}
